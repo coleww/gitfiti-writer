7936da2348c61f3672b982dbd050b2c97abedb0e
Cole Willsea <coleww@gmail.com>
2015-10-04 14:29:35 -0700

    does stuff, yeah

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var alphabet = require('./alphabet')
+
 module.exports = function (str) {
-  return 'hello ' + str
+  var gitfiti = [[],[],[],[],[],[],[]]
+  str.split(" ").forEach(function (word) {
+    word.split("").forEach(function (charmander) {
+      var rows = alphabet[charmander]
+      if (rows) for (var i = 0; i < 7;  gitfiti[i].push(rows[i]) && gitfiti[i++].push([0])) {}
+    })
+    for (var i = 0; i < 7;  gitfiti[i++].push([0, 0, 0])) {}
+  })
+
+  return gitfiti.map(function (row) {
+    return row.reduce(function (a, b) {return a.concat(b)})
+  })
 }
+
